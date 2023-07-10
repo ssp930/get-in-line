21d57f31477d628e49e6a0377190a7c8a43289b4
SSP <sungsu930@naver.com>
2023-07-10 17:06:14 +0900

    버젼 복구 완료
    git 오류 - view controller 복구.

---
+++ src/main/java/com/fastcampus/getinline/controller/AdminController.java
@@ -0,0 +1,38 @@
+package com.fastcampus.getinlinenew.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping("/places")
+    public String adminPlaces() {
+        return "admin/places";
+    }
+
+    @GetMapping("/places/{placeId}")
+    public String adminPlaceDetail(@PathVariable Integer placeId) {
+        return "admin/place-id";
+    }
+
+    @GetMapping("/events")
+    public String adminEvents() {
+        return "admin/events";
+    }
+
+    @GetMapping("/events/{eventId}")
+    public String adminEventDetail(@PathVariable Integer eventId) {
+        return "admin/event-id";
+    }
+
+
+
+
+
+
+
+}
---
+++ src/main/java/com/fastcampus/getinline/controller/AuthController.java
@@ -0,0 +1,20 @@
+package com.fastcampus.getinlinenew.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "auth/login";
+    }
+
+    @GetMapping("/sign-in")
+    public String signIn() {
+        return "auth/sign-in";
+    }
+
+
+}
